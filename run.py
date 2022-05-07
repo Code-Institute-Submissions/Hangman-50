c6eabbe0433797a107fb27b4571a5c4d9b5ae999
Simon 'Cooper' Coupe <Simonjosephcoupe@gmail.com>
2022-05-07 16:05:11 +0000

    Add while loop and if else logic blocked out code

--- run.py
+++ run.py
@@ -27,9 +27,38 @@ def play(word):
     guessed_words = []
     # number of guesses left before game is lost, (head, torso, left and right arms, left and right legs)
     lives = 6
+    # prints welcome message, ASCII art and current word status
     print("Let's play a game of Hangman!")
-    
+    print(display_hangman(lives))
+    print(word_completion)
+    print("\n")
+    #loop for game logic
+    while not guessed and lives > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        # ..if guess is one letter
+        if len(guess) == 1 and guess.isalpha():
+
+        #..if guess is the same length of the chosen word and all letters
+        elif len(guess) == len(word) and guess.isalpha()
+
+        else:
+            print("That is not a valid guess. Please try again.")
+        
+
+
+
+
+
+
+
+
+
+
 def display_hangman(lives):
+    """
+    adds visual ASCII art to player to indicate
+    how many lives they have used/left
+    """
     stages = [
                 """
                     --------
@@ -95,4 +124,4 @@ def display_hangman(lives):
                     -
                 """,
     ]
-    return stages[tries]
+    return stages[lives]
