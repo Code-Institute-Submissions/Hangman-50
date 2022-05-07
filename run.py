3c10af22b28330d64251dac56e63cb64f677c54a
Simon 'Cooper' Coupe <Simonjosephcoupe@gmail.com>
2022-05-07 14:37:48 +0000

    Import random module, set up function to randomly pick a word from words.py

--- run.py
+++ run.py
@@ -2,3 +2,11 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    print(word)
+
+get_word()
